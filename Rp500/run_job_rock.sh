94e6d6bc7c0dec577f3f682477723d33b4e9153f
Hyeuk Ryu <hyeuk.ryu@ga.gov.au>
2019-09-08 16:31:00 +1000

    added

--- Rp500/run_job_rock.sh
+++ Rp500/run_job_rock.sh
@@ -1,25 +1,28 @@
-#PBS -m e
-#PBS -P y57
-#PBS -q normalbw
-#PBS -l walltime=00:10:00
-<<<<<<< HEAD
-#PBS -l ncpus=56
-#PBS -l mem=256GB
-=======
-#PBS -l ncpus=28
-#PBS -l mem=128GB
->>>>>>> 2d9fd18c083841dd10dd1a666de760618446d5ff
-#PBS -l wd
-#PBS -N job_rock
-#PBS -l jobfs=10GB
-#PBS -l other=hyperthread
+#!/bin/bash
+eval "$(conda shell.bash hook)"
+for var in rock soil
+do
+    for rp in Rp500 Rp1000 Rp2500
+    do
+        ini_file=./$rp/job_$var.ini
+        log_file=./$rp/job_$var.log
 
-module load openquake/3.5
-oq-ini.all.sh
-<<<<<<< HEAD
-oq engine --run job_rock.ini --exports csv >&  job_rock.log && oq engine --export-output 4 ./output
-=======
-oq engine --run job_rock.ini --exports csv,xml >&  job_rock.log && oq engine --export-output 1 ./output
->>>>>>> 2d9fd18c083841dd10dd1a666de760618446d5ff
-oq-end.sh
+        if [ -f "$ini_file" ]; then
+            oq engine --run $ini_file --exports csv >&  $log_file
+            JOB_ID=$(head -n 1 $log_file | grep -Po '(?<=\#).+?')
+            echo "$rp, $var, $JOB_ID"
+            id_rlz=$(tail -n 4 $log_file | grep -Po '\d+(?= \| Realizations)')
+            echo $id_rlz
+
+            oq engine --export-output $id_rlz ./output 
+            oq show events > ./$rp/output/eid2rlz_$JOB_ID.csv
+
+            conda activate base
+            python ../code/average_gmf.py -i ./output/gmf-data_$JOB_ID.csv
+            conda deactivate
+        else
+            echo "$ini_file does not exist"
+        fi
+        done
+done
 
